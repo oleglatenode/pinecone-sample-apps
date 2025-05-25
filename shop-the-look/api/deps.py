0fa50eadf0f53f56d927fce4757efd196bbb8ce4
dbim85 <75034777+dbim85@users.noreply.github.com>
2025-05-25 17:16:50 +0300

    Delete shop-the-look directory

--- shop-the-look/api/deps.py
+++
@@ -1,5 +0,0 @@
-from pinecone import Pinecone
-from api.config import settings
-
-pc = Pinecone(api_key=settings.api_key, source_tag="pinecone:stl_sample_app")
-index = pc.Index(settings.index_name)
