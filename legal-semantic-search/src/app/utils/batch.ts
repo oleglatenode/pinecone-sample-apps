189484b7add6c777c576952513b3ed97010c8658
dbim85 <75034777+dbim85@users.noreply.github.com>
2025-05-25 17:16:27 +0300

    Delete legal-semantic-search directory

--- legal-semantic-search/src/app/utils/batch.ts
+++
@@ -1,7 +0,0 @@
-export function batchArray<T>(array: T[], batchSize: number): T[][] {
-  const batches = [];
-  for (let i = 0; i < array.length; i += batchSize) {
-    batches.push(array.slice(i, i + batchSize));
-  }
-  return batches;
-}
